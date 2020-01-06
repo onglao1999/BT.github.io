535362b81d7b78e66baab85d4eb77b2ab2d0960a
Ong_Lao_Ngao <onglaongao@OngLaoNgaosMBP.net.fpt>
2020-01-06 21:21:41 +0700

    BT trên mạng

---
+++ BT_Mang/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  BT_Mang
+//
+//  Created by Ong_Lao_Ngao on 12/31/19.
+//  Copyright © 2019 Ong_Lao_Ngao. All rights reserved.
+//
+
+import Foundation
+
+//Viết chương trình nhập mảng các số thực và xuất các phần tử âm trong mảng.
+soATM()
+print("---")
+
+//Viết chương trình nhập vào 1 mảng gồm n số nguyên. Sắp xếp lại mảng theo thứ tự tăng dần và in ra màn hình
+sapXep()
+print("---")
+
+// Viết chương trình nhập vào một mảng A gồm n số nguyên và nhập thêm 1 số nguyên x . Kiểm tra xem phần tử x có trong mảng A hay không
+kiemTra()
+print("---")
+
+//Nhập mảng A có n phần tử . Tìm phần tử xuất hiện nhiều nhất trong mảng
+timPT()
+print("---")
+
+//Viết chương trình nhập vào một mảng số nguyên có N phần tử theo thứ tự giảm dần và số M. Chèn số M vào mảng sao cho mảng vẫn có thứ tự giảm dần. In mảng sau khi chèn
+chenMang()
+print("---")
+
+//Nhập vào mảng 1 chiều gồm 1 dãy số nguyên có N phần tử. Hãy xoá các phần tử trùng nhau trong mảng và in kết quả ra màn hình
+ptTrungNhau()
+print("---")
+
+
+// viết chương trình kiểm tra số chính phương trong mảng 1 chiều
+soChinhPhuong()
+print("---")
+
+//Viết chương trình nhập vào số nguyên dương N và dãy số A gồm N số nguyên . Hãy sắp xếp dãy số trên sao cho các số chẵn chia hết cho 3 lên đầu
+soCCHC3()
+print("---")
